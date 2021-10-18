53123a3eb10ad1ee9eb62e8a64c82032bb10c22c
Rebekah <rdleamons@gmail.com>
2021-10-18 16:12:48 -0500

    Working on Second Playable

--- Penelope's_Odyssey/Assets/Scripts/SubtitleManager.cs
+++ Penelope's_Odyssey/Assets/Scripts/SubtitleManager.cs
@@ -36,7 +36,7 @@ public class SubtitleManager : MonoBehaviour
         textBox.GetComponent<Text>().text = "";
 
         yield return new WaitForSeconds(1);
-        textBox.GetComponent<Text>().text = "Click to stop & sniff for objects that could lead to Penelope's owner.";
+        textBox.GetComponent<Text>().text = "Click & hold to stop and sniff for objects that could lead to Penelope's owner.";
         yield return new WaitForSeconds(5);
         textBox.GetComponent<Text>().text = "";
 
