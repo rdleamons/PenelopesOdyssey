ee5d9104b36d073f560e56aefb2c146c0bf2a275
D'Raven McCoy <dravenmccoy1@gmail.com>
2021-11-03 17:26:12 -0500

    Added Options menu to gameplay scene, and made working volume slider

---
+++ Penelope's_Odyssey/Assets/Scripts/OptionsMenuScripts.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.Audio;
+
+public class OptionsMenuScripts : MonoBehaviour
+{
+    [Tooltip("Root GameObject of the menu used to toggle its activation")]
+    public GameObject MenuRoot;
+
+
+   public AudioSource MyAudioSource;
+    //Value from the slider, and it converts to volume level
+   public float VolumeSliderValue;
+
+    void Start()
+    {
+        MenuRoot.SetActive(false);
+
+        //Initiate the Slider value to half way
+        VolumeSliderValue = 0.5f;
+        //Fetch the AudioSource from the GameObject
+        MyAudioSource = GetComponent<AudioSource>();
+        //Play the AudioClip attached to the AudioSource on startup
+        MyAudioSource.Play();
+    }
+
+    void OnGUI()
+    {
+        MyAudioSource.volume = VolumeSliderValue;
+    }
+
+
+    public void CloseOptionsMenu()
+    {
+        SetOptionsMenuActivation(false);
+    }
+    public void OpenOptionsMenu()
+    {
+        SetOptionsMenuActivation(true);
+    }
+
+
+    void SetOptionsMenuActivation(bool active)
+    {
+        MenuRoot.SetActive(active);
+
+        if (MenuRoot.activeSelf)
+        {
+            Cursor.visible = true;
+            Time.timeScale = 0f;
+
+            EventSystem.current.SetSelectedGameObject(null);
+        }
+        else
+        {
+            Time.timeScale = 1f;
+        }
+
+    }
+
+
+
+
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!MenuRoot.activeSelf && Input.GetMouseButtonDown(0))
+        {
+            Cursor.visible = false;
+        }
+
+
+    }
+}
