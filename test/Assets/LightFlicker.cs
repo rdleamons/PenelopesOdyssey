697f23f3bd181055f078857f51ab16b316668e87
Kota047 <61256205+Kota047@users.noreply.github.com>
2021-11-19 22:18:22 -0600

    Added Lights
    Added lights to the cars and streetlights. couldn't add the flicker

---
+++ test/Assets/LightFlicker.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFlicker : MonoBehaviour
+{
+    public bool isFlickering = false;
+    public float timeDelay;
+    public float timeRange;
+    void Update()
+    {
+        if (isFlickering == false)
+        {
+            StartCoroutine(FlickeringLight());
+        }
+    }
+
+    IEnumerator FlickeringLight()
+    {
+        isFlickering = true;
+        this.gameObject.GetComponent<Light>().enabled = false;
+        timeDelay = Random.Range(0.1f, 2.5f);
+        yield return new WaitForSeconds(timeDelay);
+        this.gameObject.GetComponent<Light>().enabled = true;
+        timeDelay = Random.Range(0.1f, 2.5f);
+        yield return new WaitForSeconds(timeDelay);
+        isFlickering = false;
+    }
+}
