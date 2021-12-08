7f53726c8b63a1dccde783d97ee4c754abf38137
Rebekah <rdleamons@gmail.com>
2021-12-08 10:49:46 -0600

    Added end cut scene

--- Penelope's_Odyssey/Assets/Scripts/CutScenes.cs
+++ Penelope's_Odyssey/Assets/Scripts/CutScenes.cs
@@ -6,10 +6,15 @@ using UnityEngine.SceneManagement;
 public class CutScenes : MonoBehaviour
 {
     public bool goToGame;
+    public bool goToCredits;
+
+    private string current;
 
     void Start()
     {
+        current = SceneManager.GetActiveScene().name;
         goToGame = false;
+        goToCredits = false;
         Cursor.lockState = CursorLockMode.Locked;
     }
 
@@ -17,9 +22,17 @@ public class CutScenes : MonoBehaviour
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Return))
-            goToGame = true;
+        {
+            if (current == "OpenCut")
+                goToGame = true;
+            else if (current == "EndCut")
+                goToCredits = true;
+        }
 
         if (goToGame)
             SceneManager.LoadScene("ThirdPlayable");
+
+        if (goToCredits)
+            SceneManager.LoadScene("CreditScroll");
     }
 }
